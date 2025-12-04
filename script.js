d1fc41a97415c8b028a15b5a37260799b67ab71b
NKG-09 <200639623+NKG-09@users.noreply.github.com>
2025-12-04 23:32:13 +0530

    Add constructor for Book object

---
+++ script.js
@@ -0,0 +1,50 @@
+const books = [];
+const booksDisplay = document.querySelector("#books");
+
+function Book(title, author, pages, completed) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.completed = completed;
+  this.id = crypto.randomUUID();
+  
+  this.el = document.createElement("div");
+  this.el.classList.add("book");
+  
+  const firstSection = document.createElement("div");
+
+  const titleDisplay = document.createElement("h1");
+  titleDisplay.textContent = this.title;
+  titleDisplay.classList.add("title");
+  firstSection.appendChild(titleDisplay);
+
+  const authorDisplay = document.createElement("h2");
+  authorDisplay.textContent = this.author;
+  authorDisplay.classList.add("author");
+  firstSection.appendChild(authorDisplay);
+
+  const secondSection = document.createElement("div");
+
+  const pagesDisplay = document.createElement("p");
+  pagesDisplay.textContent = `${this.pages} pages`;
+  pagesDisplay.classList.add("pages");
+  secondSection.appendChild(pagesDisplay);
+
+  const completedButton = document.createElement("button");
+  completedButton.textContent = (this.completed ? "Completed" : "Not complete");
+  completedButton.classList.add("completed");
+  completedButton.addEventListener("click", () => {
+    this.completed = !this.completed;
+    completedButton.textContent = (this.completed ? "Completed" : "Not complete");
+  });
+  secondSection.appendChild(completedButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Remove";
+  deleteButton.classList.add("delete-button");
+  deleteButton.addEventListener("click", () => removeBookFromLibrary(this.id));
+  secondSection.appendChild(deleteButton);
+
+  this.el.appendChild(firstSection);
+  this.el.appendChild(secondSection);
+}
